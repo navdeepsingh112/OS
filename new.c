3c1132573b54305f26cfa5c470e6bc11439c9738
navdeepsingh112 <76862649+navdeepsingh112@users.noreply.github.com>
2024-01-31 10:18:44 +0530

    Update new.c

--- new.c
+++ new.c
@@ -73,3 +73,89 @@ disp('Optimal solution');
 disp(P(c2, :))  %Optimal solution
 disp('Optimal value');
 disp(min(sum))   %Optimal value
+
+
+
+
+
+
+
+
+
+
+//from here
+
+clc;
+clear all;
+
+% Define the matrices A and b
+A = [2 4; 3 5];
+b = [8; 15];
+y = 0:1:10;
+
+% Calculate x1 values for each constraint
+x11 = (b(1) - A(1, 1) * y) / A(1, 2);
+x21 = (b(2) - A(2, 1) * y) / A(2, 2);
+
+% Ensure non-negative values
+x11 = max(0, x11);
+x21 = max(0, x21);
+
+% Plot the constraints
+plot(y, x11, 'r', y, x21, 'b');
+title('Graphical Method');
+xlabel('x2');
+ylabel('x1');
+
+% Finding corner points of the constraints
+cornerpts = [];
+c1 = find(y == 0);
+c2 = find(x11 == 0);
+line1 = [y([c1, c2]); x11([c1, c2])]';
+
+c1 = find(y == 0);
+c2 = find(x21 == 0);
+line2 = [y([c1, c2]); x21([c1, c2])]';
+
+cornerpts = unique([line1; line2], 'rows');
+
+% Finding intersection points
+pt = [];
+for i = 1:size(A, 1)
+    A1 = A(i, :);
+    b1 = b(i);
+    for j = i+1:size(A, 1)
+        A2 = A(j, :);
+        b2 = b(j);
+        P = [A1; A2];
+        Q = [b1; b2];
+        X = P \ Q;
+        if all(X >= 0)
+            pt = [pt, X];
+        end
+    end
+end
+
+allpts = unique([pt'; cornerpts], 'rows');
+
+% Assuming all points are valid as no additional constraints are given
+P = allpts;
+
+% Find max/min
+sum = 3 * P(:, 1) + 2 * P(:, 2);
+
+[maxVal, maxIdx] = max(sum);
+[minVal, minIdx] = min(sum);
+
+% Displaying the results
+disp('MAX');
+disp('Optimal solution:');
+disp(P(maxIdx, :));  % Optimal solution
+disp('Optimal value');
+disp(maxVal);   % Optimal value
+
+disp('MIN');
+disp('Optimal solution:');
+disp(P(minIdx, :));  % Optimal solution
+disp('Optimal value');
+disp(minVal);   % Optimal value
